c8f0287c4994afbecf91bc4c300ed3144e028af9
Guilherme Figueira <79333503+Alarket@users.noreply.github.com>
2023-02-06 15:36:56 -0300

    Initial & Final Commit
    End of Project

---
+++ pages/about-us.js
@@ -0,0 +1,20 @@
+const AboutUsPage = () => {
+    return <div>
+    <h1>About us Page</h1>
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit"</p>
+    <h2>adipiscing elit, sed do eiusmod tempor</h2>
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit"</p>
+    <ul className='about_us_list'>
+        <li>
+        <h3>adipiscing elit, sed do eiusmod tempor</h3>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit"</p>
+        </li>
+        <li>
+        <h3>adipiscing elit, sed do eiusmod tempor</h3>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit"</p>
+        </li>
+    </ul>
+    </div>;
+};
+
+export default AboutUsPage;
---
+++ src/components/footer/footer.js
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return(
+    <footer>
+    <p>
+      Time to Code
+    </p>
+  </footer>
+  )
+}
